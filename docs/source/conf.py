3bc50f8bd151055027ccc671cfde2eb9ac1dc92c
Peter Urban <peter.urban@ugent.be>
2022-05-23 17:03:03 +0200

    added license information

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2022 Peter Urban, Ghent University
+#
+# SPDX-License-Identifier: CC0-1.0
+
 # Configuration file for the Sphinx documentation builder.
 
 # -- Project information
--- docs/source/usage.rst
+++ docs/source/usage.rst
@@ -1,3 +1,7 @@
+.. SPDX-FileCopyrightText: 2022 Peter Urban, Ghent University
+..
+.. SPDX-License-Identifier: MPL-2.0
+
 Usage
 =====
 
