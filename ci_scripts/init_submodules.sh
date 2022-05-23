34b981cdd63e83b90eba8d55a7402ee43c19c605
Peter Urban <peter.urban@ugent.be>
2022-05-23 17:21:16 +0200

    ci scripts

---
+++ ci_scripts/init_submodules.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+# SPDX-FileCopyrightText: 2022 Peter Urban, Ghent University
+#
+# SPDX-License-Identifier: CC0-1.0
+
+git submodule update --init --recursive
---
+++ ci_scripts/reuse_examples.sh
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2022 Peter Urban, Ghent University
+#
+# SPDX-License-Identifier: CC0-1.0
+
+alias reuse_mpl='reuse addheader --copyright="Peter Urban, Ghent University" --license="MPL-2.0"'
+alias reuse_cc0='reuse addheader --copyright="Peter Urban, Ghent University" --license="CC0-1.0"'
+
