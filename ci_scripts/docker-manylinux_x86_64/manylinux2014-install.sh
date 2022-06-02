852c72125dfe3a1b08e450cefc453265b335e16b
Peter Urban <peter.urban@ugent.be>
2022-06-02 20:39:50 +0200

    moving to cibuildwheels

---
+++ ci_scripts/docker-manylinux_x86_64/manylinux2014-install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# install source build dependcies
+yum install -y bzip2 curl zlib
+
+export BOOST_VERSION="1.74.0"
+export BOOST_VERSION_="1_74_0"
+export BOOST_MODULES="--with-filesystem"
+
+echo get https://boostorg.jfrog.io/artifactory/main/release/${BOOST_VERSION}/source/boost_${BOOST_VERSION_}.tar.bz2
+
+# -- install package versions from source --
+# BOOST
+curl -O -L https://boostorg.jfrog.io/artifactory/main/release/${BOOST_VERSION}/source/boost_${BOOST_VERSION_}.tar.bz2 && \
+    tar -xf boost_${BOOST_VERSION_}.tar.bz2 && \
+    cd boost_* && \
+    ./bootstrap.sh  && \
+    ./b2 install --variant=release --threading=multi --link=static ${BOOST_MODULES} && \
+    cd .. && \
+    rm -rv boost*
+
+# install build requirements
+yum install -y git git-lfs
+
+
+# install dependecies (c++)
+yum install -y ccache pkgconfig cmake libcurl-devel
