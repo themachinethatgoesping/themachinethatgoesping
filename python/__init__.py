52158c4dd516fbde960e35a12e225af3cebeb132
Peter Urban <peter.urban@ugent.be>
2022-06-10 10:00:22 +0200

    moved __init__.py to main dir

---
+++ python/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2022 GEOMAR Helmholtz Centre for Ocean Research Kiel
+# SPDX-FileCopyrightText: 2022 Peter Urban, Ghent University
+#
+# SPDX-License-Identifier: MPL-2.0
+
+#import the module functions into the current namespace
+from themachinethatgoesping import tools
+from themachinethatgoesping import tools_ext
