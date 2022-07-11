f5743f2b9004ba2c93fa06b3d6835fc27f98a6b5
Peter Urban <peter.urban@ugent.be>
2022-07-11 15:54:42 +0200

    updating subprojects

--- ci_scripts/rebuild_docker.sh
+++ ci_scripts/rebuild_docker.sh
@@ -14,7 +14,7 @@ echo $PAT | docker login ghcr.io --username themachinethatgoesping --password-st
 docker build docker-ubuntu -t ghcr.io/themachinethatgoesping/ubuntu-dep --no-cache
 docker push ghcr.io/themachinethatgoesping/ubuntu-dep
 
-docker build docker-manylinux_x86_64 -t ghcr.io/themachinethatgoesping/manylinux_x86_64-dep #--no-cache
+docker build docker-manylinux_x86_64 -t ghcr.io/themachinethatgoesping/manylinux_x86_64-dep --no-cache
 docker push ghcr.io/themachinethatgoesping/manylinux_x86_64-dep
 
 #docker build docker-manylinux_aarch64 -t ghcr.io/themachinethatgoesping/manylinux_aarch64-dep #--no-cache
