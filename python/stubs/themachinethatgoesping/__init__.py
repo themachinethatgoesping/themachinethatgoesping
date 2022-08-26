2a698b28b68b4fbe122bc520a1e73ad8eec3b6d7
Peter Urban <peter.urban@ugent.be>
2022-08-26 14:17:16 +0200

    added xtensor capability + version bumnp

--- python/stubs/themachinethatgoesping/__init__.py
+++ python/stubs/themachinethatgoesping/__init__.py
@@ -22,6 +22,6 @@ __all__ = [
 
 __submodule = '../'
 modules_installed = ['tools', 'tools_ext', 'navigation', 'gridding']
-pydev_install_module_paths = ['/ssd/local/lib/python3/dist-packages/themachinethatgoesping', '/ssd/src/themachinethatgoesping/themachinethatgoesping/subprojects/meta/..//tools/python/themachinethatgoesping', '/ssd/src/themachinethatgoesping/themachinethatgoesping/subprojects/meta/..//gridding/python/themachinethatgoesping']
+pydev_install_module_paths = ['/ssd/local/lib/python3.10/site-packages/themachinethatgoesping', '/ssd/src/themachinethatgoesping/themachinethatgoesping/subprojects/meta/..//tools/python/themachinethatgoesping', '/ssd/src/themachinethatgoesping/themachinethatgoesping/subprojects/meta/..//gridding/python/themachinethatgoesping']
 pydev_install_modules = ['tools', 'navigation', 'gridding', '../']
 pydev_install_src_path = '/ssd/src/themachinethatgoesping/themachinethatgoesping/subprojects/meta/../'
