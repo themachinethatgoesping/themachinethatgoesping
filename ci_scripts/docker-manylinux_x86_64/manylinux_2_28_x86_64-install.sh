66d5e6bcb5f3636a281287068c41a96eb82a1013
Peter Urban <peter.urban@ugent.be>
2022-08-30 11:42:21 +0200

    fixed manylinux builds  - updated to manylinux 2_28  - use boost 1.80  - build boost static libs with -fPIC (to allow them to be used in shared modules)  - add ccache (not avaliable in alma linux)

---
+++ ci_scripts/docker-manylinux_x86_64/manylinux_2_28_x86_64-install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# install source build dependencies
+yum install -y bzip2 curl zlib
+
+#export BOOST_VERSION="1.74.0"
+#export BOOST_VERSION_="1_74_0"
+export BOOST_VERSION="1.80.0"
+export BOOST_VERSION_="1_80_0"
+export BOOST_MODULES="--with-iostreams"
+
+export CCACHE_VERSION="4.6.3"
+
+
+# -- install package versions from source --
+# BOOST
+echo get https://boostorg.jfrog.io/artifactory/main/release/${BOOST_VERSION}/source/boost_${BOOST_VERSION_}.tar.bz2
+
+curl -O -L https://boostorg.jfrog.io/artifactory/main/release/${BOOST_VERSION}/source/boost_${BOOST_VERSION_}.tar.bz2 && \
+    tar -xf boost_${BOOST_VERSION_}.tar.bz2 && \
+    cd boost_* && \
+    ./bootstrap.sh  && \
+    ./b2 install --variant=release --threading=multi --link=static ${BOOST_MODULES} --prefix=/usr/local cxxflags=-fPIC cflags=-fPIC && \
+    cd .. && \
+    /usr/bin/rm -r boost*
+
+
+# -- install ccache () --
+echo get https://github.com/ccache/ccache/releases/download/v${CCACHE_VERSION}/ccache-${CCACHE_VERSION}-linux-x86_64.tar.xz
+
+curl -O -L https://github.com/ccache/ccache/releases/download/v${CCACHE_VERSION}/ccache-${CCACHE_VERSION}-linux-x86_64.tar.xz
+tar -xf ccache-${CCACHE_VERSION}-linux-x86_64.tar.xz
+cd ccache-${CCACHE_VERSION}-linux-x86_64
+mv ccache /usr/local/bin/
+cd ..
+/usr/bin/rm -r ccache*
+
+# install build requirements
+#yum install -y git git-lfs ccache
+
+
+# install dependencies (c++)
+yum install -y pkgconfig cmake libcurl-devel
