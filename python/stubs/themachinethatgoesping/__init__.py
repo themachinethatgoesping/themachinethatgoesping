337756ce91fa8ab09fea8ea39934bbc4b60a271f
Peter Urban <peter.urban@ugent.be>
2022-11-23 18:27:51 +0100

    update submodules

--- python/stubs/themachinethatgoesping/__init__.py
+++ python/stubs/themachinethatgoesping/__init__.py
@@ -22,9 +22,9 @@ __all__ = [
 ]
 
 
-__modules_installed__ = [('tools', '0.14.1'), ('navigation', '0.8.1'), ('echosounders', '0.9.5'), ('gridding', '@PROJECT_VERSION@')]
+__modules_installed__ = [('tools', '0.14.1'), ('navigation', '0.8.1'), ('echosounders', '0.9.6'), ('gridding', '@PROJECT_VERSION@')]
 __submodule = '../'
-__version__ = '0.4.2'
+__version__ = '0.4.4'
 pydev_install_module_paths = ['/ssd/local/lib/python3.10/site-packages/themachinethatgoesping', '/ssd/src/themachinethatgoesping/themachinethatgoesping/subprojects/meta/..//tools/python/themachinethatgoesping', '/ssd/src/themachinethatgoesping/themachinethatgoesping/subprojects/meta/..//gridding/python/themachinethatgoesping']
 pydev_install_modules = ['tools', 'navigation', 'gridding', '../']
 pydev_install_src_path = '/ssd/src/themachinethatgoesping/themachinethatgoesping/subprojects/meta/../'
