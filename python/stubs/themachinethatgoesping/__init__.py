d2864badc359939db3b1dd30ce9334a4eff589fd
Peter Urban <peter.urban@ugent.be>
2022-11-25 13:34:19 +0100

    Switch to stock meson-python (>=0.11)

--- python/stubs/themachinethatgoesping/__init__.py
+++ python/stubs/themachinethatgoesping/__init__.py
@@ -22,9 +22,9 @@ __all__ = [
 ]
 
 
-__modules_installed__ = [('tools', '0.14.2'), ('navigation', '0.8.3'), ('echosounders', '0.10.0'), ('gridding', '@PROJECT_VERSION@')]
+__modules_installed__ = [('tools', '0.14.2'), ('navigation', '0.8.3'), ('echosounders', '0.10.1'), ('gridding', '@PROJECT_VERSION@')]
 __submodule = '../'
-__version__ = '0.4.4'
+__version__ = '0.4.6'
 pydev_install_module_paths = ['/ssd/local/lib/python3.10/site-packages/themachinethatgoesping', '/ssd/src/themachinethatgoesping/themachinethatgoesping/subprojects/meta/..//tools/python/themachinethatgoesping', '/ssd/src/themachinethatgoesping/themachinethatgoesping/subprojects/meta/..//gridding/python/themachinethatgoesping']
 pydev_install_modules = ['tools', 'navigation', 'gridding', '../']
 pydev_install_src_path = '/ssd/src/themachinethatgoesping/themachinethatgoesping/subprojects/meta/../'
