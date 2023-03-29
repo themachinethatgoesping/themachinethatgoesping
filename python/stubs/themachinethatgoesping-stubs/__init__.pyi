261546448c8a258fa9e99a511e6fafabc0cc1d54
Peter Urban <peter.urban@ugent.be>
2023-03-29 10:03:20 +0200

    minor version bump

--- python/stubs/themachinethatgoesping-stubs/__init__.pyi
+++ python/stubs/themachinethatgoesping-stubs/__init__.pyi
@@ -22,7 +22,7 @@ __all__ = [
 ]
 
 
-__modules_installed__ = [('tools', '0.16.3'), ('navigation', '0.9.2'), ('echosounders', '0.16.12'), ('gridding', '@PROJECT_VERSION@')]
+__modules_installed__ = [('tools', '0.16.4'), ('navigation', '0.9.2'), ('echosounders', '0.16.12'), ('gridding', '@PROJECT_VERSION@')]
 __submodule = '../'
 __version__ = '0.6.6'
 pydev_install_module_paths = ['/home/ssd/local/lib/python3.10/site-packages/themachinethatgoesping', '/ssd/src/themachinethatgoesping/themachinethatgoesping/subprojects/meta/..//tools/python/themachinethatgoesping', '/ssd/src/themachinethatgoesping/themachinethatgoesping/subprojects/meta/..//gridding/python/themachinethatgoesping']
