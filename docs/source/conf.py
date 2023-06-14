36ee454e8b8e2c154a8ec973216038b612581c07
Peter Urban <peter.urban@ugent.be>
2023-06-14 09:30:29 +0200

    revert theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,8 +53,8 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-#html_theme = 'sphinx_rtd_theme'
-html_theme = 'furo'
+html_theme = 'sphinx_rtd_theme'
+#html_theme = 'furo'
 
 html_static_path = ['_static']
 html_logo  = 'themachinethatgoesping.svg'
