a6d991690023810de6fde1cc064e61e9a2fb0137
Peter Urban <peter.urban@ugent.be>
2023-06-21 22:28:28 +0200

    update hinnant_date to git commit to fix windows building

--- python/stubs/themachinethatgoesping/__init__.py
+++ python/stubs/themachinethatgoesping/__init__.py
@@ -22,9 +22,9 @@ __all__ = [
 ]
 
 
-__modules_installed__ = [('tools', '0.18.2'), ('navigation', '0.10.4'), ('echosounders', '0.17.11'), ('gridding', '@PROJECT_VERSION@')]
+__modules_installed__ = [('tools', '0.18.3'), ('navigation', '0.10.5'), ('echosounders', '0.17.12'), ('gridding', '@PROJECT_VERSION@')]
 __submodule = '../'
-__version__ = '0.6.14'
+__version__ = '0.6.15'
 pydev_install_module_paths = ['/home/ssd/local/lib/python3.11/site-packages/themachinethatgoesping', '/ssd/src/themachinethatgoesping/themachinethatgoesping/subprojects/meta/..//tools/python/themachinethatgoesping', '/ssd/src/themachinethatgoesping/themachinethatgoesping/subprojects/meta/..//gridding/python/themachinethatgoesping']
 pydev_install_modules = ['tools', 'navigation', 'gridding', '../']
 pydev_install_src_path = '/ssd/src/themachinethatgoesping/themachinethatgoesping/subprojects/meta/../'
