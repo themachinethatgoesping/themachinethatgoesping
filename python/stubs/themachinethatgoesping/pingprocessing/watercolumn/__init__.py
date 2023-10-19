7d03d4e6402f3dc907996bcaeffc0da6fd327b8d
Peter Urban <peter.urban@ugent.be>
2023-10-19 17:44:05 +0200

    fix: print correct file number on navigation merge failure

--- python/stubs/themachinethatgoesping/pingprocessing/watercolumn/__init__.py
+++
@@ -1,4 +0,0 @@
-from __future__ import annotations
-from . import helper
-from . import make_image
-__all__ = ['helper', 'make_image']
--- python/stubs/themachinethatgoesping/pingprocessing/watercolumn/make_image.py
+++
@@ -1,5 +0,0 @@
-from __future__ import annotations
-from themachinethatgoesping.pingprocessing.watercolumn.make_image.make_wci import make_wci
-from themachinethatgoesping.pingprocessing.watercolumn.make_image.make_wci import make_wci_dual_head
-from themachinethatgoesping.pingprocessing.watercolumn.make_image.make_wci import make_wci_stack
-__all__ = ['make_wci', 'make_wci_dual_head', 'make_wci_stack']
