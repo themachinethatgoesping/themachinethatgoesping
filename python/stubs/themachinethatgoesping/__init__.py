504996a8bb966b8d86655f142e3ad60ad518f083
Peter Urban <peter.urban@ugent.be>
2024-02-15 20:46:48 +0100

    v0.18.1 -see CHANGELOG.MD

--- python/stubs/themachinethatgoesping/__init__.py
+++ python/stubs/themachinethatgoesping/__init__.py
@@ -24,7 +24,7 @@ def version():
     ...
 __modules_installed__: list = [('tools_cppy', '0.22.1'), ('tools', '@PROJECT_VERSION@'), ('scripts', '@PROJECT_VERSION@'), ('algorithms', '0.5.7'), ('navigation', '0.15.0'), ('echosounders_cppy', '0.35.1'), ('echosounders', '0.35.1'), ('pingprocessing_cppy', '0.5.2'), ('pingprocessing', '@PROJECT_VERSION@'), ('gridding', '@PROJECT_VERSION@')]
 __submodule: str = '../'
-__version__: str = '0.18.0'
+__version__: str = '0.18.1'
 pydev_install_module_paths: list = ['/home/ssd/local/lib/python3.12/site-packages/themachinethatgoesping', '/ssd/src/themachinethatgoesping/themachinethatgoesping/subprojects/meta/..//tools/python/themachinethatgoesping', '/ssd/src/themachinethatgoesping/themachinethatgoesping/subprojects/meta/..//gridding/python/themachinethatgoesping', '/ssd/src/themachinethatgoesping/themachinethatgoesping/subprojects/meta/..//echosounders/python/themachinethatgoesping', '/ssd/src/themachinethatgoesping/themachinethatgoesping/subprojects/meta/..//pingprocessing/python/themachinethatgoesping']
 pydev_install_modules: list = ['tools', 'navigation', 'gridding', 'echosounders', 'pingprocessing', '../']
 pydev_install_src_path: str = '/ssd/src/themachinethatgoesping/themachinethatgoesping/subprojects/meta/../'
