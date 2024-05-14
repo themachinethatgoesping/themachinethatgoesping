de366dbe2ed47894450e8b2227b879f8756f4152
Peter Urban <peter.urban@ugent.be>
2024-05-14 18:40:45 +0200

    added wciviewer widget

--- python/stubs/themachinethatgoesping/pingprocessing/widgets/__init__.py
+++ python/stubs/themachinethatgoesping/pingprocessing/widgets/__init__.py
@@ -1,6 +1,15 @@
+from IPython.core.display_functions import display
 from __future__ import annotations
 from collections import OrderedDict
+import ipywidgets as ipywidgets
 from ipywidgets.widgets.widget_int import IntProgress
+from matplotlib import pyplot as plt
+import themachinethatgoesping as Ping
+from themachinethatgoesping.pingprocessing.watercolumn.helper import make_image_helper as mi_hlp
+from themachinethatgoesping.pingprocessing.watercolumn import image as mi
 from themachinethatgoesping.pingprocessing.widgets.tqdmwidget import TqdmWidget
+from themachinethatgoesping.pingprocessing.widgets.wciviewer import WCIViewer
+from time import time
 from . import tqdmwidget
-__all__ = ['IntProgress', 'OrderedDict', 'TqdmWidget', 'tqdmwidget']
+from . import wciviewer
+__all__ = ['IntProgress', 'OrderedDict', 'Ping', 'TqdmWidget', 'WCIViewer', 'display', 'ipywidgets', 'mi', 'mi_hlp', 'plt', 'time', 'tqdmwidget', 'wciviewer']
