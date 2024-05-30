289002960de6662717b32bfd039258b2e6ceb476
Peter Urban <peter.urban@ugent.be>
2024-05-30 10:56:01 +0200

    v0.22.1 minor updates and typo fixes

--- python/stubs/themachinethatgoesping/pingprocessing/widgets/__init__.py
+++ python/stubs/themachinethatgoesping/pingprocessing/widgets/__init__.py
@@ -4,12 +4,14 @@ from collections import OrderedDict
 import ipywidgets as ipywidgets
 from ipywidgets.widgets.widget_int import IntProgress
 from matplotlib import pyplot as plt
+import numpy as np
 import themachinethatgoesping as Ping
 from themachinethatgoesping.pingprocessing.watercolumn.helper import make_image_helper as mi_hlp
 from themachinethatgoesping.pingprocessing.watercolumn import image as mi
 from themachinethatgoesping.pingprocessing.widgets.tqdmwidget import TqdmWidget
 from themachinethatgoesping.pingprocessing.widgets.wciviewer import WCIViewer
 from time import time
+import types as types
 from . import tqdmwidget
 from . import wciviewer
-__all__ = ['IntProgress', 'OrderedDict', 'Ping', 'TqdmWidget', 'WCIViewer', 'display', 'ipywidgets', 'mi', 'mi_hlp', 'plt', 'time', 'tqdmwidget', 'wciviewer']
+__all__ = ['IntProgress', 'OrderedDict', 'Ping', 'TqdmWidget', 'WCIViewer', 'display', 'ipywidgets', 'mi', 'mi_hlp', 'np', 'plt', 'time', 'tqdmwidget', 'types', 'wciviewer']
