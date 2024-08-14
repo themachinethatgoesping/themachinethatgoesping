3fb497f24f6242c0b40e66b433882bb59c6d419a
Peter Urban <peter.urban@ugent.be>
2024-08-14 13:26:15 +0200

    update pybind11 -> 2.13.3

--- python/stubs/themachinethatgoesping/__init__.py
+++ python/stubs/themachinethatgoesping/__init__.py
@@ -5,6 +5,8 @@ themachinethatgoesping
 Enable quantitative processing of multibeam and singlebeam echosounder systems
 """
 from __future__ import annotations
+from pkgutil import extend_path as __extend_path__
+import sys as __internal_module_sys__
 from . import algorithms
 from . import echosounders
 from . import echosounders_cppy
@@ -15,10 +17,14 @@ from . import pingprocessing_cppy
 from . import scripts
 from . import tools
 from . import tools_cppy
-__all__ = ['algorithms', 'echosounders', 'echosounders_cppy', 'gridding', 'modules', 'navigation', 'pingprocessing', 'pingprocessing_cppy', 'scripts', 'tools', 'tools_cppy', 'version']
+__all__ = ['algorithms', 'echosounders', 'echosounders_cppy', 'gridding', 'modules', 'navigation', 'pingprocessing', 'pingprocessing_cppy', 'pydev_install_module_paths', 'pydev_install_modules', 'pydev_install_src_path', 'scripts', 'tools', 'tools_cppy', 'version']
 def modules():
     ...
 def version():
     ...
-__modules_installed__: list = [('tools_cppy', '0.25.0'), ('tools', '@PROJECT_VERSION@'), ('scripts', '@PROJECT_VERSION@'), ('algorithms', '0.8.0'), ('navigation', '0.17.0'), ('echosounders_cppy', '0.40.0'), ('echosounders', '0.40.0'), ('pingprocessing_cppy', '0.8.3'), ('pingprocessing', '@PROJECT_VERSION@'), ('gridding', '0.2.0')]
+__modules_installed__: list = [('tools_cppy', '0.25.1'), ('tools', '@PROJECT_VERSION@'), ('scripts', '@PROJECT_VERSION@'), ('algorithms', '0.8.0'), ('navigation', '0.17.0'), ('echosounders_cppy', '0.40.0'), ('echosounders', '0.40.0'), ('pingprocessing_cppy', '0.8.3'), ('pingprocessing', '@PROJECT_VERSION@'), ('gridding', '@PROJECT_VERSION@')]
+__submodule: str = '../'
 __version__: str = '0.24.1'
+pydev_install_module_paths: list = ['/home/ssd/local/lib/python3.12/site-packages/themachinethatgoesping', '/ssd/src/themachinethatgoesping/themachinethatgoesping/subprojects/meta/..//tools/python/themachinethatgoesping', '/ssd/src/themachinethatgoesping/themachinethatgoesping/subprojects/meta/..//gridding/python/themachinethatgoesping', '/ssd/src/themachinethatgoesping/themachinethatgoesping/subprojects/meta/..//echosounders/python/themachinethatgoesping', '/ssd/src/themachinethatgoesping/themachinethatgoesping/subprojects/meta/..//pingprocessing/python/themachinethatgoesping']
+pydev_install_modules: list = ['tools', 'navigation', 'gridding', 'echosounders', 'pingprocessing', '../']
+pydev_install_src_path: str = '/ssd/src/themachinethatgoesping/themachinethatgoesping/subprojects/meta/../'
