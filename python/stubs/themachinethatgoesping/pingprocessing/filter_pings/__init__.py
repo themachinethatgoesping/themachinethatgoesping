878fb6d91b5902b1513c04684b5fb937ce141c34
Peter Urban <peter.urban@ugent.be>
2024-11-22 17:12:40 +0100

    update submodules - see CHANGELOG.MD

--- python/stubs/themachinethatgoesping/pingprocessing/filter_pings/__init__.py
+++ python/stubs/themachinethatgoesping/pingprocessing/filter_pings/__init__.py
@@ -4,5 +4,7 @@ from themachinethatgoesping.pingprocessing.filter_pings.by_files import by_files
 from themachinethatgoesping.pingprocessing.filter_pings.by_files import by_folders
 from themachinethatgoesping.pingprocessing.filter_pings.by_region import by_latlon
 from themachinethatgoesping.pingprocessing.filter_pings.by_time import by_time
+from themachinethatgoesping.pingprocessing.filter_pings.by_time_list import by_ping_times
+from themachinethatgoesping.pingprocessing.filter_pings.by_time_list import by_time_list
 from . import by_region
-__all__ = ['by_features', 'by_files', 'by_folders', 'by_latlon', 'by_region', 'by_time']
+__all__ = ['by_features', 'by_files', 'by_folders', 'by_latlon', 'by_ping_times', 'by_region', 'by_time', 'by_time_list']
