b386345d6c046de0fc156528a7f117cadcc8d268
Peter Urban <peter.urban@ugent.be>
2024-12-02 22:25:45 +0100

    update doc and submodules

--- python/stubs/themachinethatgoesping/pingprocessing/watercolumn/echograms/__init__.py
+++ python/stubs/themachinethatgoesping/pingprocessing/watercolumn/echograms/__init__.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 from themachinethatgoesping.pingprocessing.watercolumn.echograms.echodata import EchoData
+from themachinethatgoesping.pingprocessing.watercolumn.echograms.echogrambuilder import EchogramBuilder
 from themachinethatgoesping.pingprocessing.watercolumn.echograms.echogramgroup import EchogramGroup
 from themachinethatgoesping.pingprocessing.watercolumn.echograms.echogramgroup import create_echogram_groups
-from themachinethatgoesping.pingprocessing.watercolumn.echograms.echoproxy import EchoProxy
 from . import echodata
+from . import echogrambuilder
 from . import echogramgroup
-from . import echoproxy
-__all__ = ['EchoData', 'EchoProxy', 'EchogramGroup', 'create_echogram_groups', 'echodata', 'echogramgroup', 'echoproxy']
+from . import echolayer
+__all__ = ['EchoData', 'EchogramBuilder', 'EchogramGroup', 'create_echogram_groups', 'echodata', 'echogrambuilder', 'echogramgroup', 'echolayer']
--- python/stubs/themachinethatgoesping/pingprocessing/widgets/echogramviewer.py
+++ python/stubs/themachinethatgoesping/pingprocessing/widgets/echogramviewer.py
@@ -13,8 +13,10 @@ import typing
 __all__ = ['EchogramViewer', 'asyncio', 'display', 'echograms', 'ipywidgets', 'mdates', 'np', 'plt', 'theping', 'time', 'types']
 class EchogramViewer:
     __firstlineno__: typing.ClassVar[int] = 15
-    __static_attributes__: typing.ClassVar[tuple] = ('args_plot', 'axes', 'box_buttons', 'box_sliders', 'clear_button', 'cmap', 'colorbar', 'display_progress', 'echogram_axes', 'echogramdata', 'extents_background', 'fig', 'fig_events', 'high_res_extents', 'high_res_images', 'images_background', 'loop', 'mapables', 'names', 'nechograms', 'output', 'pingline', 'pingviewer', 'progress', 'task', 'update_button', 'w_interpolation', 'w_vmax', 'w_vmin', 'x_axis_name', 'xlim', 'y_axis_name', 'ylim')
-    def __init__(self, echogramdata, name = 'Echogram', names = None, figure = None, progress = None, show = True, cmap = 'YlGnBu_r', **kwargs):
+    __static_attributes__: typing.ClassVar[tuple] = ('args_plot', 'args_plot_layer', 'axes', 'box_buttons', 'box_sliders', 'clear_button', 'cmap', 'cmap_layer', 'colorbar', 'display_progress', 'echogram_axes', 'echogramdata', 'extents_background', 'fig', 'fig_events', 'high_res_extents', 'high_res_images', 'images_background', 'layer_extents', 'layer_images', 'loop', 'mapables', 'names', 'nechograms', 'output', 'pingline', 'pingviewer', 'progress', 'task', 'update_button', 'w_interpolation', 'w_vmax', 'w_vmin', 'x_axis_name', 'xlim', 'y_axis_name', 'ylim')
+    def __init__(self, echogramdata, name = 'Echogram', names = None, figure = None, progress = None, show = True, cmap = 'YlGnBu_r', cmap_layer = 'jet', **kwargs):
+        ...
+    def callback_view(self):
         ...
     def clear_output(self, event = 0):
         ...
