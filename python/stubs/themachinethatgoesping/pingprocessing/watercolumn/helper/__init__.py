86aa31bdd2568d23fb626756bdc6ece7f4c7af24
Peter Urban <peter.urban@ugent.be>
2024-12-12 23:48:34 +0100

    update submodules - see CHANGELOG.MD

--- python/stubs/themachinethatgoesping/pingprocessing/watercolumn/helper/__init__.py
+++ python/stubs/themachinethatgoesping/pingprocessing/watercolumn/helper/__init__.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from themachinethatgoesping.pingprocessing.watercolumn.helper.select_get_wci_image import apply_pss
 from themachinethatgoesping.pingprocessing.watercolumn.helper.select_get_wci_image import select_get_wci_image
 from . import make_image_helper
-__all__ = ['make_image_helper', 'select_get_wci_image']
+__all__ = ['apply_pss', 'make_image_helper', 'select_get_wci_image']
