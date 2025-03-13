9d5fe06a41819d1a48e7e0094ab6705f603f5809
Peter Urban <peter.urban@ugent.be>
2025-03-13 14:54:50 +0100

    initial spine origin functions

--- python/stubs/themachinethatgoesping/algorithms/pointprocessing/bubblestreams.py
+++ python/stubs/themachinethatgoesping/algorithms/pointprocessing/bubblestreams.py
@@ -19,19 +19,19 @@ class ZSpine:
         """
     @staticmethod
     @typing.overload
-    def from_point_cloud(x: numpy.ndarray[numpy.float64], y: numpy.ndarray[numpy.float64], z: numpy.ndarray[numpy.float64], weights: numpy.ndarray[numpy.float64], n_quantiles: int) -> ZSpine:
+    def from_point_cloud(x: numpy.ndarray[numpy.float64], y: numpy.ndarray[numpy.float64], z: numpy.ndarray[numpy.float64], weights: numpy.ndarray[numpy.float64], n_quantiles: int, is_altitude: bool = False) -> ZSpine:
         ...
     @staticmethod
     @typing.overload
-    def from_point_cloud(x: numpy.ndarray[numpy.float64], y: numpy.ndarray[numpy.float64], z: numpy.ndarray[numpy.float64], weights: numpy.ndarray[numpy.float32], n_quantiles: int) -> ZSpine:
+    def from_point_cloud(x: numpy.ndarray[numpy.float64], y: numpy.ndarray[numpy.float64], z: numpy.ndarray[numpy.float64], weights: numpy.ndarray[numpy.float32], n_quantiles: int, is_altitude: bool = False) -> ZSpine:
         ...
     @staticmethod
     @typing.overload
-    def from_point_cloud(x: numpy.ndarray[numpy.float32], y: numpy.ndarray[numpy.float32], z: numpy.ndarray[numpy.float32], weights: numpy.ndarray[numpy.float64], n_quantiles: int) -> ZSpine:
+    def from_point_cloud(x: numpy.ndarray[numpy.float32], y: numpy.ndarray[numpy.float32], z: numpy.ndarray[numpy.float32], weights: numpy.ndarray[numpy.float64], n_quantiles: int, is_altitude: bool = False) -> ZSpine:
         ...
     @staticmethod
     @typing.overload
-    def from_point_cloud(x: numpy.ndarray[numpy.float32], y: numpy.ndarray[numpy.float32], z: numpy.ndarray[numpy.float32], weights: numpy.ndarray[numpy.float32], n_quantiles: int) -> ZSpine:
+    def from_point_cloud(x: numpy.ndarray[numpy.float32], y: numpy.ndarray[numpy.float32], z: numpy.ndarray[numpy.float32], weights: numpy.ndarray[numpy.float32], n_quantiles: int, is_altitude: bool = False) -> ZSpine:
         ...
     def __copy__(self) -> ZSpine:
         ...
@@ -46,10 +46,10 @@ class ZSpine:
         hash function implemented using binary_hash
         """
     @typing.overload
-    def __init__(self) -> None:
+    def __init__(self, is_altitude: bool = False) -> None:
         ...
     @typing.overload
-    def __init__(self, x: list[float], y: list[float], z: list[float]) -> None:
+    def __init__(self, x: list[float], y: list[float], z: list[float], is_altitude: bool = False) -> None:
         ...
     def __repr__(self) -> str:
         """
@@ -61,21 +61,33 @@ class ZSpine:
         """
         Return object information as string
         """
+    def add_point(self, x: float, y: float, z: float) -> None:
+        ...
+    def add_points(self, x: list[float], y: list[float], z: list[float]) -> None:
+        ...
     def copy(self) -> ZSpine:
         """
         return a copy using the c++ default copy constructor
         """
-    def get_spine(self, n_points: int) -> numpy.ndarray[numpy.float64]:
+    def estimate_origin(self, bottom_z: float, slope_modifier: float = 1.0) -> None:
         ...
-    def get_spine_points(self) -> tuple[list[float], list[float], list[float]]:
+    def get_is_altitude(self) -> bool:
+        ...
+    def get_origin(self) -> tuple[float, float, float] | None:
+        ...
+    def get_spine(self, n_points: int, with_origin: bool = True) -> numpy.ndarray[numpy.float64]:
+        ...
+    def get_spine_points(self, with_origin: bool = True) -> tuple[list[float], list[float], list[float]]:
         ...
     def get_x_interpolator(self) -> themachinethatgoesping.tools_cppy.vectorinterpolators.AkimaInterpolator:
         ...
-    @typing.overload
-    def get_xy(self, z: numpy.ndarray[numpy.float64]) -> numpy.ndarray[numpy.float64]:
+    def get_xy(self, z: float) -> tuple[float, float]:
         ...
     @typing.overload
-    def get_xy(self, z: numpy.ndarray[numpy.float32]) -> numpy.ndarray[numpy.float32]:
+    def get_xy_vec(self, z: numpy.ndarray[numpy.float64]) -> numpy.ndarray[numpy.float64]:
+        ...
+    @typing.overload
+    def get_xy_vec(self, z: numpy.ndarray[numpy.float32]) -> numpy.ndarray[numpy.float32]:
         ...
     def get_y_interpolator(self) -> themachinethatgoesping.tools_cppy.vectorinterpolators.AkimaInterpolator:
         ...
@@ -91,6 +103,10 @@ class ZSpine:
         """
         Print object information
         """
+    def reset_origin(self) -> None:
+        ...
+    def set_origin(self, x: float, y: float, z: float) -> None:
+        ...
     def to_binary(self, resize_buffer: bool = True) -> bytes:
         """
         convert object to bytearray
