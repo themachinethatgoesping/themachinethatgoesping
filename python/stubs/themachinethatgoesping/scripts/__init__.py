6d593876c9c637406873364c675b19a468bba09e
Peter Urban <peter.urban@ugent.be>
2025-03-29 22:08:23 +0100

    updated submodules - CHANGELOG.MD

--- python/stubs/themachinethatgoesping/scripts/__init__.py
+++ python/stubs/themachinethatgoesping/scripts/__init__.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 from . import mbes
+from . import nearfield
 from . import oceanographic
 from . import plot_tools
 from . import sbes
-__all__ = ['mbes', 'oceanographic', 'plot_tools', 'sbes']
+__all__ = ['mbes', 'nearfield', 'oceanographic', 'plot_tools', 'sbes']
 __version__: str = '@PROJECT_VERSION@'
---
+++ python/stubs/themachinethatgoesping/scripts/nearfield.py
@@ -0,0 +1,84 @@
+from __future__ import annotations
+from matplotlib import pyplot as plt
+import numpy as np
+__all__ = ['minimum_recommended_calibration_range', 'nearfield_range_sbes', 'np', 'plot_nearfield_range', 'plot_recommended_sphere_range', 'plt']
+def _compute_kappa(wavelength):
+    ...
+def _compute_wavelength(frequency, sound_speed):
+    ...
+def _estimate_diameter(kappa, beam_width_degrees):
+    ...
+def _nearfield_range_sbes_1(transducer_diameter, wavelength):
+    ...
+def _nearfield_range_sbes_2(transducer_diameter, wavelength):
+    ...
+def _nearfield_range_sbes_3(beam_width_degrees, frequency, sound_speed):
+    ...
+def minimum_recommended_calibration_range(beam_width_degrees, frequency_khz, sound_speed_ms = 1470, model = 2):
+    """
+    
+    Calculate the minimum recommended calibration range for a single beam echo sounder.
+    
+    Parameters
+    ----------
+    beam_width_degrees : float
+        Beam width in degrees.
+    frequency_khz : float
+        Frequency of the sound wave in kHz.
+    sound_speed_ms : float, optional
+        Speed of sound in water in m/s, by default 1470.
+    model : int, optional
+        Model to use for calculation (1 or 2), by default 2.
+    
+    Model 1: Simmonds and MacLennan (2005) in Demer et al. (2015)
+    Model 2: Medwin and Clay (1998) in Demer et al. (2015)
+    
+    Returns
+    -------
+    float
+        Minimum recommended calibration range in meters.
+    
+    Raises
+    ------
+    ValueError
+        If the model is not 1 or 2.
+    """
+def nearfield_range_sbes(beam_width_degrees, frequency_khz, sound_speed_ms = 1470, model = 2):
+    """
+    
+    Calculate the nearfield range for a single beam echo sounder. (Circular piston transducer)
+    
+    Parameters
+    ----------
+    beam_width_degrees : float
+        Beam width in degrees.
+    frequency_khz : float
+        Frequency of the sound wave in kHz.
+    sound_speed_ms : float, optional
+        Speed of sound in water in m/s, by default 1470.
+    model : int, optional
+        Model to use for calculation (1 or 2), by default 2.
+        
+    Model 1: Simmonds and MacLennan (2005) in Demer et al. (2015)
+    Model 2: Medwin and Clay (1998) in Demer et al. (2015)
+    
+    Returns
+    -------
+    float
+        Nearfield range in meters.
+    
+    Raises
+    ------
+    ValueError
+        If the model is not 1 or 2.
+    """
+def plot_nearfield_range(frequencies_kHz = [333, 200, 120, 70, 38, 18, 12], beamwidth_range = (2, 12), yticks = [1, 1.5, 2, 3, 4, 6, 8, 10, 15, 20], sound_speed_ms = 1470, model = 2, ax = None, name = ''):
+    """
+    
+    Plot the estimated nearfield range for different frequencies and beam widths.
+    """
+def plot_recommended_sphere_range(frequencies_kHz = [333, 200, 120, 70, 38, 18, 12], beamwidth_range = (2, 12), yticks = [1, 1.5, 2, 3, 4, 6, 8, 10, 15, 20], sound_speed_ms = 1470, model = 2, ax = None, name = ''):
+    """
+    
+    Plot the recommended sphere range for different frequencies and beam widths.
+    """
